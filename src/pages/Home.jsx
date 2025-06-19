41a73467fd7e642744126ad73bb1864636ba1336
Onalerona <onzwanejhb024@student.wethinkcode.co.za>
2025-06-19 14:45:18 +0200

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () =>{
     useEffect(() => {
         // Sending a get request to the home page and storing the json strings in vars to customize later
         // Remember that .data is the actual payload from my api in the console!
-        axios.get("http://localhost:9090/home").then(response => setcontent(response.data)).catch(err => console.error("Error fetching home page data"))
+        axios.get("http://localhost:9090").then(response => setcontent(response.data)).catch(err => console.error("Error fetching home page data"))
 
     }, []);
 
